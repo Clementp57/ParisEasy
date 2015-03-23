9e8683959d8ef9a47019c9211bcc794843e0eacc
Maxime Martin <contact@maximemartin.fr>
2015-03-23 11:19:27 +0100

    getLocation on the home controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,26 @@
 angular.module('parisEasy.controllers', [])
 
-.controller('HomeCtrl', function($scope) {});
+.controller('HomeCtrl', function($scope, $cordovaGeolocation, $ionicPlatform) {
+
+	$scope.getLocation = function(){
+
+		console.info('Getting location...');
+
+		$ionicPlatform.ready(function() {
+			var posOptions = {timeout: 10000, enableHighAccuracy: false};
+			  $cordovaGeolocation
+			    .getCurrentPosition(posOptions)
+			    .then(function (position) {
+			      var lat  = position.coords.latitude;
+			      var long = position.coords.longitude;
+			      console.info(lat +" "+ long);
+			    }, function(err) {
+			      console.info(err);
+			  	});
+		});
+	}
+
+});
 
 // .controller('ChatsCtrl', function($scope, Chats) {
 //   $scope.chats = Chats.all();
