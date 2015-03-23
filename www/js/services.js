8bf3913679456725b70e2d89d4b9fead4ac5e6b4
Clement Peyrabere <clementpeyrabere@me.com>
2015-03-23 12:24:18 +0100

    Service done

--- www/js/services.js
+++ www/js/services.js
@@ -86,6 +86,34 @@ angular.module('parisEasy.services', [])
                 }); 
 
             return deferred.promise;
+        },
+        getGeoActivities: function (catId, tag, created, start, end, lat, lon, radius, offset, limit) {
+            var deferred = $q.defer();
+            var start = new Date().getTime(); //performance analyse
+            
+            var url = baserUrl+"QueFaire/get_geo_activites/?token="+token;
+            url+= "&cid="+catId;
+            url+= "&tag="+tag;
+            url+= "&created="+created;
+            url+= "&start="+start;
+            url+= "&end="+end;
+            url+= "&lat="+lat;
+            url+= "&lon="+lon;
+            url+= "&radius="+radius;
+            url+= "&offset="+offset;
+            url+= "&limit="+limit;
+
+            var result = $http.get(url).then(
+                function (response) {
+                    deferred.resolve(result);
+                    console.log('time taken for request: ' + (new Date().getTime() - start) + 'ms'); //debug
+                    return response.data;
+                },
+                function () {
+                    //$rootScope.notify('La connexion avec le serveur à échouée. Essayez de recharger la page.','error')
+                }); 
+
+            return deferred.promise;
         }
     };
 }])
